e94e1b44156fa948b90955298995fc2754aea2f0
Yehor Borkov <yehor.borkov@gmail.com>
2025-03-12 18:54:04 +0000

    feat: update flake outputs to include the gcc9 overlay

--- flake.nix
+++ flake.nix
@@ -5,11 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+        config = { };
+      };
+    in
+    {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-  };
+      # Older versions of pulp-platform projects fail to build under GCC 10/11
+      overlays.default = final: prev: {
+        gcc = prev.gcc9;
+      };
+    };
 }
