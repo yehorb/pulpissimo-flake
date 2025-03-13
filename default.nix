d79e75b5cac7134e74672efc54184f0023266236
Yehor Borkov <yehor.borkov@gmail.com>
2025-03-13 08:46:11 +0000

    refactor: use the preferred pname and version convention

--- default.nix
+++ default.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 
 # Older versions of pulp-platform projects fail to build under GCC 10/11
-pkgs.gcc9Stdenv.mkDerivation {
-  name = "pulp-riscv-gnu-toolchain";
+pkgs.gcc9Stdenv.mkDerivation rec {
+  pname = "pulp-riscv-gnu-toolchain";
+  version = "1.0.16";
   src = pkgs.fetchFromGitHub {
     owner = "pulp-platform";
     repo = "pulp-riscv-gnu-toolchain";
-    tag = "v1.0.16";
+    tag = "v${version}";
     hash = "sha256-RSnzVGBH/zm2cHhkaDdg1aZKiClbx/iwM8olKsaw/Eo=";
     fetchSubmodules = true;
   };
