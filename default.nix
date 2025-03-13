316f9864f2736f2baa584bb936d5035ab229a979
Yehor Borkov <yehor.borkov@gmail.com>
2025-03-13 09:28:46 +0000

    fix(nix build): add gmp-6.1.0 external dependency

--- default.nix
+++ default.nix
@@ -5,12 +5,19 @@
   flex,
   bison,
   texinfo,
+  fetchurl,
   ...
 }:
 
 let
   # Older versions of pulp-platform projects fail to build under GCC 10/11
   stdenv = gcc9Stdenv;
+  gmp = fetchurl rec {
+    pname = "gmp";
+    version = "6.1.0";
+    url = "ftp://gcc.gnu.org/pub/gcc/infrastructure/${pname}-${version}.tar.bz2";
+    hash = "sha256-SYRJqZTv66UniFwQQFmTQnmV0/hrh2jYzfjZ3Xxrc+g=";
+  };
 in
 stdenv.mkDerivation rec {
   pname = "pulp-riscv-gnu-toolchain";
@@ -28,4 +35,11 @@ stdenv.mkDerivation rec {
     bison
     texinfo
   ];
+  env = {
+    DISTDIR = "$out/distfiles";
+  };
+  preConfigure = ''
+    mkdir -p $DISTDIR
+    cp ${gmp} $DISTDIR
+  '';
 }
