82b90173f9124f645832ab38d0747d37da26eaff
Yehor Borkov <yehor.borkov@gmail.com>
2025-03-13 18:30:03 +0000

    feat: try add ccache

--- flake.nix
+++ flake.nix
@@ -9,9 +9,40 @@
     { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+        config = { };
+      };
     in
     {
       packages.${system}.default = pkgs.callPackage ./default.nix { };
+
+      overlays.default = final: prev: {
+        ccacheWrapper = prev.ccacheWrapper.override {
+          extraConfig = ''
+            export CCACHE_COMPRESS=1
+            export CCACHE_DIR="/nix/var/cache/ccache"
+            export CCACHE_UMASK=007
+            if [ ! -d "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' does not exist"
+              echo "Please create it with:"
+              echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+              echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+              echo "====="
+              exit 1
+            fi
+            if [ ! -w "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+              echo "Please verify its access permissions"
+              echo "====="
+              exit 1
+            fi
+          '';
+        };
+        gcc9Stdenv = final.ccacheStdenv.override { stdenv = prev.gcc9Stdenv; };
+      };
     };
 }
